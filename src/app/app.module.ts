ccc9a80c0c499038912ba23234157c9223dc6bda
Bankole Samuel <bankolesamuel175@gmail.com>
2019-11-11 02:59:54 +0100

    create route for the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'list', component: ListComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
